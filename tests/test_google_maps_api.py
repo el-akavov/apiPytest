94e6933e541a3c9463f052a248628f3ffbbdff24
Eldar Akavov <akavoveldar3005@gmail.com>
2022-12-25 19:11:14 +0300

    feat(test_google_maps_api): add test for PUT method

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -12,5 +12,11 @@ class Test_create_place():
         check_post = result_post.json()
         place_id = check_post.get("place_id")
 
-        print("Метод GET")
+        print("Метод GET POST")
+        result_get: Response = Google_maps_api.get_new_place(place_id)
+
+        print("Метод PUT")
+        result_put: Response = Google_maps_api.put_new_place(place_id)
+
+        print("Метод GET PUT")
         result_get: Response = Google_maps_api.get_new_place(place_id)
