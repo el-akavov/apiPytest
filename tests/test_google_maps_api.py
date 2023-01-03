1273d1e30a6b44baa5935e1645b947514ed2e287
Eldar Akavov <akavoveldar3005@gmail.com>
2023-01-04 00:17:20 +0300

    feat(test_google_maps_api.py): add check_status_code method

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,5 +1,5 @@
 from requests import Response
-
+from utils.checking import Cheking
 from utils.api import Google_maps_api
 
 # Создание, изменение, удадение новой локации
@@ -11,18 +11,23 @@ class Test_create_place():
         result_post: Response = Google_maps_api.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
+        Cheking.check_status_code(result_post, 200)
 
         print("Метод GET POST")
         result_get: Response = Google_maps_api.get_new_place(place_id)
+        Cheking.check_status_code(result_get, 200)
 
         print("Метод PUT")
         result_put: Response = Google_maps_api.put_new_place(place_id)
+        Cheking.check_status_code(result_put, 200)
 
         print("Метод GET PUT")
         result_get: Response = Google_maps_api.get_new_place(place_id)
-
+        Cheking.check_status_code(result_get, 200)
         print("Метод DELETE")
         result_delete: Response = Google_maps_api.delete_new_place(place_id)
+        Cheking.check_status_code(result_delete, 200)
 
         print("Метод GET DELETE")
         result_get: Response = Google_maps_api.get_new_place(place_id)
+        Cheking.check_status_code(result_get, 404)
