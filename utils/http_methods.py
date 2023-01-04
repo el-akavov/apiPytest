a6da043c326cfbbe0f6b3824a27dab3deb8afb6d
Eldar Akavov <akavoveldar3005@gmail.com>
2023-01-05 01:21:04 +0300

    add logger recording methods

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -1,5 +1,7 @@
 import requests
 
+from utils.logger import Logger
+
 """Список http методов"""
 
 class Http_methods:
@@ -8,20 +10,28 @@ class Http_methods:
 
     @staticmethod
     def get(url):
+        Logger.add_request(url, method="GET")
         result = requests.get(url, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method="POST")
         result = requests.post(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method="PUT")
         result = requests.put(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method="DELETE")
         result = requests.delete(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
